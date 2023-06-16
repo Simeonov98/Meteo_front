9e110cb18e70bdf19f2556eb6eafdac37134ddd3
Simeonov98 <sunko1993@gmail.com>
2023-06-17 01:37:27 +0300

    Work on layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,12 @@ import withMT from "@material-tailwind/react/utils/withMT";
 export default withMT({
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors:{
+      'base': '#213363',
+      'firstLayer': '#17594A',
+      'secondLayer': '#8EAC50',
+      'thirdLayer': '#D3D04F'
+    },
     extend: {},
   },
   plugins: [],
